6b5f45868ed56d7fc4d2c8cfa611f7a65728799b
Mykhailo Shamrai <mike.shamrai@gmail.com>
2025-02-08 20:30:51 +0100

    small cleaning

--- cmake-build-debug-system/third_party/glm/glmConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(GLM_VERSION "0.9.8")
-set(GLM_INCLUDE_DIRS "C:/Users/Mike Shamrai/CLionProjects/CG4_Scene/third_party/glm")
-
-if (NOT CMAKE_VERSION VERSION_LESS "3.0")
-    include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
-endif()
